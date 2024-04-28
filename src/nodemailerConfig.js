7254601b771d50529d513c178e08d86a1584af2c
Sebastian-Giraldo <86446864+Sebastian-Giraldo@users.noreply.github.com>
2024-04-28 14:46:43 -0500

    Server running and send emails

--- src/nodemailerConfig.js
+++ src/nodemailerConfig.js
@@ -1,15 +1,26 @@
-const nodemailer = require('nodemailer');
-require('dotenv').config();
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 // Configuracion de Nodemailer
 const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: false,
-    auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-    },
-})
+
+  host: 'smtp.gmail.com',
+  port: 587,
+  auth: {
+    user: 'MAIL_USERNAME',
+    pass: 'MAIL_PASSWORD'
+  }
+  // service: 'gmail',
+  // auth: {
+  //   type: 'OAuth2',
+  //   user: process.env.MAIL_USERNAME,
+  //   pass: process.env.MAIL_PASSWORD,
+  //   clientSecret: process.env.OAUTH_CLIENT_SECRET,
+  //   refreshToken: process.env.OAUTH_REFRESH_TOKEN
+  // }
+});
 
 module.exports = transporter;
+
+
+
--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 // Simple server whit EXpress.js and MondoDB Atlas
 
 // IMPORTS
+const { Resend } = require("resend");
 const express = require("express");
 const bodyParser = require("body-parser");
-const transporter = require("./nodemailerConfig");
 const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT;
-
+const resend = new Resend(process.env.API_KEY_RESEND);
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -50,32 +50,65 @@ run().catch(console.dir);
 
 // Routes of acces methods
 app.get("/", async (req, res) => {
-  try{
-      const persons = await client.db("test").collection("persons").find().toArray();
-      res.status(200).json(persons);
-  }catch(err){
-      res.status(500).json({error: err.message})
+  try {
+    const persons = await client
+      .db("test")
+      .collection("persons")
+      .find()
+      .toArray();
+    res.status(200).json(persons);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
-})
+});
 
-app.post("/", async (req, res) => {
-  try{
-      const newPerson = req.body;
-      const result = await client.db("test").collection("persons").insertOne(newPerson);
-      res.status(201).json(result);
-  }catch(err){
-      res.status(500).json({error: err.message})
+// Función asincrónica para enviar el correo electrónico
+async function sendEmail(newPerson) {
+  try {
+    // Envía el correo electrónico
+    const { data, error } = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: [process.env.MAIL_USERNAME],
+      subject: "Estoy interesado en realizar un proyecto contigo!",
+      html: `<p>Name: ${newPerson.name}</p>
+            <p>Email: ${newPerson.email}</p>
+            <p>Message: ${newPerson.message}</p>`,
+    });
+
+    // Maneja los resultados
+    if (error) {
+      console.error({ error });
+    } else {
+      console.log({ data });
+    }
+  } catch (error) {
+    console.error(error);
   }
-})
+}
 
-app.put('/contact', async function (req, res) {
+app.post("/pages/contact/contact", async (req, res) => {
+  try {
+    const newPerson = req.body;
+    const result = await client
+      .db("test")
+      .collection("persons")
+      .insertOne(newPerson);
+    res.status(201).json(result);
+
+    sendEmail(newPerson);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.put("/", async function (req, res) {
   try {
     const personId = req.body.id;
     const updatedPerson = req.body;
-    const result = await client.db("test").collection("persons").updateOne(
-      { _id: new ObjectId(personId) },
-      { $set: updatedPerson }
-    );
+    const result = await client
+      .db("test")
+      .collection("persons")
+      .updateOne({ _id: new ObjectId(personId) }, { $set: updatedPerson });
     if (result.modifiedCount > 0) {
       res.status(200).send("Person updated successfully");
     } else {
@@ -86,10 +119,13 @@ app.put('/contact', async function (req, res) {
   }
 });
 
-app.delete('/', async function (req, res) {
+app.delete("/", async function (req, res) {
   try {
     const personId = req.body.id;
-    const result = await client.db("test").collection("persons").deleteOne({ _id: ObjectId(personId) });
+    const result = await client
+      .db("test")
+      .collection("persons")
+      .deleteOne({ _id: ObjectId(personId) });
     if (result.deletedCount > 0) {
       res.status(200).send("Person deleted successfully");
     } else {
@@ -99,7 +135,6 @@ app.delete('/', async function (req, res) {
     res.end("Person not deleted");
   }
 });
-
 
 // Server startup confirmation
 app.listen(port, () => {
